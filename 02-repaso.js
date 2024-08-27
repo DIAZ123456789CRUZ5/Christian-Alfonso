57be859f5bb8aedc4d3bdd41fdef598c891e7aea
DIAZ123456789CRUZ5 <christianalfonsodiaz18@gmail.com>
2024-08-26 19:23:35 -0600

    Christian-Actividades

---
+++ 02-repaso.js
@@ -0,0 +1,27 @@
+/*/Realizar un arreglo con dos dimenciones y 
+guardar dos arreglos internos y dar lectura a los internos*/
+
+let A=[[3,4,5,6,],[7,8,9]];
+
+
+const datosArreglo=()=>{
+    A.map((datos,index))
+}
+
+//for of
+//for in
+//for each
+//function flecha
+//whille
+
+//estructura de control parser
+
+let a=4
+let b=5
+
+if(a>b){
+    console.log('la a es mayor ${a}') ;
+}
+const comparar= a>b?"A es mayor":"B es mayor"
+console.log(comparar)
+
---
+++ act-1.js
@@ -0,0 +1,63 @@
+/*/Realizar un arreglo con dos dimenciones y guardar dos arreglos internos,
+y dar lectura a los internos (utilizando los siguientes metodos):*/
+
+//-for of
+
+
+const array = [1,2,3,4,5];
+for (const elemento of array) {
+    console.log(elemento)
+}
+//-for in 
+let matriz = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+];
+console.log("usando//for in")
+for (let i in matriz) {
+    for (let j in matriz[i]){
+        console.log(matriz[i][j]);
+    }
+}
+//-for each
+let B = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+];
+
+console.log ("usando el ciclo forEach");
+matriz.forEach(fila => {
+    fila.forEach(elemento => {
+        console.log(elemento);
+    })
+})
+//-function flecha
+let m = [ 
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+];
+
+console.log('funcion flecha:');
+m.forE
+//-whille
+let c = [
+    [1,2,3]
+    [4,5,6]
+];
+console.log ("ciclo while")
+whille(i<c.length);{
+    i++
+    let v=c[i]
+    while(j<c.length){
+        j++;
+        let = v [i][j]
+
+    }
+}
+console.log(let)
+//-estructura control parser
+
+
